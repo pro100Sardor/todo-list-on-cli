dca24d13ae804f4323d80258e13f70606f2c6109
pro100Sardor <sardorbek.juraqulov@gmail.com>
2021-01-10 23:15:00 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*******************************************************************************************\
+
+ 1. obyektning bitta propertysiga kiritilgan ma'lumotni birorbir massiv yoki obyektga saqlab qo'yish scriptini yozish
+ 2. 1 taskda yozilgan scriptdan foydalangan holda kiritilgan ma'lumotlar joylashgan arryni console chiqarish
+ 3. mavjud ma'lumotlarni o'zgartirish scriptini yozish
+ 4. Mavjud ma'lumotlarni o'chirish scriptini yozish
+ 5. protsess argv dan foydalanish
+ 6. clickni oddit telefonlarda ishlaydigan logikasiga o'xshash logika tuzish
+ 7. kodni optimallashtirish va class based qilish.
+
+\*******************************************************************************************/
+
